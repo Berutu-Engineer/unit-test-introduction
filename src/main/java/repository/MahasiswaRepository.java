4445b3548a930db2a9faaf2f3135f265863a940d
esteryb <esteryolandaberutu@gmail.com>
2024-10-01 23:55:53 +0700

    Add Repository Mahasiswa

--- src/main/java/repository/MahasiswaRepository.java
+++ src/main/java/repository/MahasiswaRepository.java
@@ -1,4 +1,37 @@
 package repository;
 
+
+import entity.Kamar;
+import entity.Mahasiswa;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MahasiswaRepository {
+    private static List<Mahasiswa> listMahasiswa;
+
+    public static void addMahasiswa(Mahasiswa mahasiswa) {
+        if (listMahasiswa == null) {
+            listMahasiswa = new ArrayList<>();
+        }
+
+        listMahasiswa.add(mahasiswa);
+    }
+
+    public static List<Mahasiswa> getAllMahasiswa() {
+        return listMahasiswa;
+    }
+
+    public static Mahasiswa getMahasiswaByIdMahasiswa(String idMahasiswa) {
+        for (Mahasiswa mahasiswa : listMahasiswa) {
+            if (idMahasiswa.equals(mahasiswa.getId())) {
+                return mahasiswa;
+            }
+        }
+        return null;
+    }
+
+    public static void emptyMahasiswa() {
+        listMahasiswa = null;
+    }
+
 }
