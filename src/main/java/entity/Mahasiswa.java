26579ae4c613c830af943da241dd5cfce7179267
esteryb <esteryolandaberutu@gmail.com>
2024-10-01 23:54:35 +0700

    Update Entity Mahasiswa

--- src/main/java/entity/Mahasiswa.java
+++ src/main/java/entity/Mahasiswa.java
@@ -3,14 +3,37 @@
  */
 package entity;
 
+import java.util.List;
+
 /**
  * @author Yosepri Disyandro Berutu (yosepri.berutu@binus.ac.id)
  * @version $Id: Mahasiswa.java , v 0.1 2024-09-28 13.54 Yosepri Disyandro Berutu Exp $$
  */
 public class Mahasiswa {
+    private String id;
     private String nim;
     private String nama;
     private String jenisKelamin;
+
+    public Mahasiswa() {
+    }
+
+    public Mahasiswa(String id, String nim, String nama, String jenisKelamin) {
+        this.id = id;
+        this.nim = nim;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
 
     public String getNim() {
         return nim;
