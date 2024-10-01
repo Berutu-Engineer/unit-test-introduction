4da8698678f5633a615656a3562da1201406f38a
esteryb <esteryolandaberutu@gmail.com>
2024-10-01 23:58:20 +0700

    Update Asrama Service Test

--- src/test/java/repository/AsramaServiceTest.java
+++ src/test/java/repository/AsramaServiceTest.java
@@ -4,6 +4,7 @@
 package repository;
 
 import entity.Asrama;
+import entity.Kamar;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import service.AsramaService;
@@ -21,6 +22,21 @@ public class AsramaServiceTest {
         Assertions.assertNull(AsramaRepository.getAllAsrama());
     }
 
+//    @Test
+//    public void testTambahAsrama_NotEmptyRequestSuccess() {
+//        AsramaRepository.emptyAsrama();
+//
+//        Asrama asrama = new Asrama();
+//        asrama.setId("Asrama-01");
+//        asrama.setNama("Asrama A");
+//        asrama.setJenis("Aspa");
+//        asrama.setJumlahKamar(10);
+//
+//        AsramaService.tambahAsrama(asrama);
+//
+//        Assertions.assertNotNull(AsramaRepository.getAllAsrama());
+//    }
+
     @Test
     public void testTambahAsrama_NotEmptyRequestSuccess() {
         AsramaRepository.emptyAsrama();
@@ -33,7 +49,42 @@ public class AsramaServiceTest {
 
         AsramaService.tambahAsrama(asrama);
 
+        Kamar kamar = new Kamar();
+        kamar.setId("Kamar-01");
+        kamar.setNomorKamar("Kamar 1");
+        kamar.setKapasitas(6);
+
+        boolean succes = AsramaService.tambahKamarKeAsrama("Asrama-01", kamar);
+
         Assertions.assertNotNull(AsramaRepository.getAllAsrama());
+    }
+
+    @Test
+    public void testTambahKamarKeAsramaPenuh() {
+        Asrama asrama = new Asrama();
+        asrama.setId("Asrama-01");
+        asrama.setNama("Asrama A");
+        asrama.setJenis("Aspa");
+        asrama.setJumlahKamar(1);
+
+        AsramaService.tambahAsrama(asrama);
+
+        Kamar kamar = new Kamar();
+        kamar.setId("Kamar-01");
+        kamar.setNomorKamar("Kamar 1");
+        kamar.setKapasitas(6);
+
+        Kamar kamar2 = new Kamar();
+        kamar.setId("Kamar-02");
+        kamar.setNomorKamar("Kamar 2");
+        kamar.setKapasitas(6);
+
+        boolean success1 = AsramaService.tambahKamarKeAsrama("Asrama-01", kamar);
+        boolean success2 = AsramaService.tambahKamarKeAsrama("Asrama-01", kamar2); // Asrama sudah penuh
+
+        Assertions.assertTrue(success1);
+        Assertions.assertFalse(success2);
+        Assertions.assertEquals(1, asrama.getListKamar().size());
     }
 
     @Test
