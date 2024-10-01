c120a856358e1b4e651d30d47e15c9eed44a4372
esteryb <esteryolandaberutu@gmail.com>
2024-10-01 20:23:19 +0700

    Add Kamar Service

--- src/main/java/service/KamarService.java
+++ src/main/java/service/KamarService.java
@@ -1,4 +1,26 @@
 package service;
 
+import entity.Kamar;
+import repository.AsramaRepository;
+import repository.KamarRepository;
+
 public class KamarService {
+
+    public static void tambahKamar(Kamar kamar) {
+        if (kamar == null) {
+            return;
+        }
+
+        kamar.setId(generateIdKamar());
+
+        KamarRepository.addKamar(kamar);
+    }
+
+    public static String generateIdKamar(){
+        if (KamarRepository.getAllKamar() == null) {
+            return "Kamar-01";
+        }
+
+        return "Kamar-0" + String.valueOf(KamarRepository.getAllKamar().size() + 1);
+    }
 }
