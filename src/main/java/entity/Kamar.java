220739c61c628969341ea7232d60178b2c0b65a3
esteryb <esteryolandaberutu@gmail.com>
2024-10-01 20:21:40 +0700

    Update Entity Kamar

--- src/main/java/entity/Kamar.java
+++ src/main/java/entity/Kamar.java
@@ -3,14 +3,36 @@
  */
 package entity;
 
+import java.util.List;
+
 /**
  * @author Yosepri Disyandro Berutu (yosepri.berutu@binus.ac.id)
  * @version $Id: Kamar.java , v 0.1 2024-09-28 13.53 Yosepri Disyandro Berutu Exp $$
  */
 public class Kamar {
-
+    private String id;
     private String nomorKamar;
     private int kapasitas;
+
+    public Kamar() {
+    }
+
+    public Kamar(String id, String nomorKamar, int kapasitas) {
+        this.id = id;
+        this.nomorKamar = nomorKamar;
+        this.kapasitas = kapasitas;
+    }
+
+    private List<Mahasiswa> listMahasiswa;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getNomorKamar() {
         return nomorKamar;
@@ -28,3 +50,5 @@ public class Kamar {
         this.kapasitas = kapasitas;
     }
 }
+
+
